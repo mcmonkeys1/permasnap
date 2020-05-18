11fa5243459d2ce8b8322d3d36a107622a6dbd84
Ros McMahon <rosmcmahon@gmail.com>
2020-05-18 19:03:08 +0100

    TODO: Splash closing too soon

--- permasnap-client/src/index.tsx
+++ permasnap-client/src/index.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-/* Redux stuff */
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react'; //delays until data retrieved
 import store, { persistor} from './redux/store'
 import { assertPermasnapAlbum } from './providers/FilesystemProvider';
-/* defineCustomElements is for camera error */
 import { defineCustomElements } from '@ionic/pwa-elements/loader'
-defineCustomElements(window);
 
+/* defineCustomElements is for camera error */
+defineCustomElements(window);
 /* prompt for permissions on app first start */
 assertPermasnapAlbum()
 
